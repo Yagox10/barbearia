c748d0796f69799c8acfa877475ca95864c8cdde
Enrique Rene <enriquerenebr@gmail.com>
2019-12-04 18:25:29 -0300

    kkkkkk

---
+++ frontend/build/precache-manifest.8d022f0fea648a7df76d14ca00ebf412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5adb9f1537242b8e0d4bbea935b0c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c8f730ec4fac0d4c457",
+    "url": "/static/css/main.4336b846.chunk.css"
+  },
+  {
+    "revision": "4a36af2bba0521a3eec4",
+    "url": "/static/js/2.ff3eb8a4.chunk.js"
+  },
+  {
+    "revision": "9c8f730ec4fac0d4c457",
+    "url": "/static/js/main.2660c2c6.chunk.js"
+  },
+  {
+    "revision": "e1032095262fb45cc0b0",
+    "url": "/static/js/runtime~main.80e9fd68.js"
+  }
+]);
