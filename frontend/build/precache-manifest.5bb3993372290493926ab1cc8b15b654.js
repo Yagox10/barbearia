b089c8f8e4630ed97287df745648f8d04c3861d5
Enrique Rene <enriquerenebr@gmail.com>
2019-12-27 13:19:10 -0300

    depoloying

---
+++ frontend/build/precache-manifest.5bb3993372290493926ab1cc8b15b654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908da167e807267b7983e74d1c7294d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdebfac58962b1e17b93",
+    "url": "/static/css/main.4336b846.chunk.css"
+  },
+  {
+    "revision": "49436501363076e48a3a",
+    "url": "/static/js/2.87f175be.chunk.js"
+  },
+  {
+    "revision": "bdebfac58962b1e17b93",
+    "url": "/static/js/main.f4044f6d.chunk.js"
+  },
+  {
+    "revision": "e1032095262fb45cc0b0",
+    "url": "/static/js/runtime~main.80e9fd68.js"
+  }
+]);
